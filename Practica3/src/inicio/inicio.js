836f7c70a4a3952e6203163189693471f1bfe23c
J0nan <jagilcarrera@gmail.com>
2021-02-20 19:31:10 +0100

    Lista principal dianamica, se lee del txt

--- Practica3/src/inicio/inicio.js
+++ Practica3/src/inicio/inicio.js
@@ -1,4 +1,16 @@
-function cargar_apartados(file){
-    var reader = new FileReader();
-    
+function cargar_apartados(ruta){
+    fetch(ruta)
+        .then(response => response.text())
+        .then(contenido => {
+            const lista = document.getElementById("lista");
+            const filas = contenido.split('\n');
+            filas.forEach(fila => {
+                let a = document.createElement("a");
+                a.setAttribute('href',"./src/"+fila+".html");
+                a.setAttribute('class',"list-group-item list-group-item-action");
+                a.textContent = fila
+                lista.appendChild(a)
+                console.log(fila)
+            });
+        });
 }
