0483f708459a5ebf8c1fdb06834da8eb64505407
J0nan <jagilcarrera@gmail.com>
2021-02-21 02:59:48 +0100

    imagen dinamica

--- Practica3/src/inicio/inicio.js
+++ Practica3/src/inicio/inicio.js
@@ -1,17 +1,17 @@
 function cargar_apartados(ruta){
     fetch(ruta)
-        .then(response => response.text())
-        .then(contenido => {
-            const lista = document.getElementById("lista");
-            const filas = contenido.split('\n');
-            filas.forEach(fila => {
-                let a = document.createElement("a");
-                fila = fila.replace(/[\r\n]+/gm, "" );
-                a.setAttribute('href',encodeURI(`./src/instruccion.html?titulo=${fila}&paso=1`));
-                a.setAttribute('class',"list-group-item list-group-item-action");
-                a.textContent = fila
-                lista.appendChild(a)
-                console.log(fila)
-            });
+    .then(response => response.text())
+    .then(contenido => {
+        const lista = document.getElementById("lista");
+        const filas = contenido.split('\n');
+        filas.forEach(fila => {
+            let a = document.createElement("a");
+            fila = fila.replace(/[\r\n]+/gm, "" );
+            a.setAttribute('href',encodeURI(`./src/instruccion.html?titulo=${fila}&paso=1`));
+            a.setAttribute('class',"list-group-item list-group-item-action");
+            a.textContent = fila
+            lista.appendChild(a)
+            console.log(fila)
         });
+    });
 }
