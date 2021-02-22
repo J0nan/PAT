b7331da3718008f892e6887967b3be8ce9c3fc97
J0nan <jagilcarrera@gmail.com>
2021-02-22 01:42:45 +0100

    pequeñas mejoras

--- Practica3/src/inicio/inicio.js
+++ Practica3/src/inicio/inicio.js
@@ -16,7 +16,7 @@ function cargar_apartados(ruta){
     })
     .catch(e => {
         console.error("Error "+e);
-        console.warn("Probando con ruta completa al archivo");
+        console.warn("Probando con ruta completa al archivo: https://j0nan.github.io/PAT/Practica3/res/contenido.txt");
         cargar_apartados('https://j0nan.github.io/PAT/Practica3/res/contenido.txt')
     });
 }
