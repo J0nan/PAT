2213e0da5a367643f7b519c78911a30effc27ca6
J0nan <jagilcarrera@gmail.com>
2021-02-23 12:35:49 +0100

    update

--- Practica3/src/inicio/inicio.js
+++ Practica3/src/inicio/inicio.js
@@ -7,7 +7,11 @@ function cargar_apartados(ruta){
         filas.forEach(fila => {
             let a = document.createElement("a");
             fila = fila.replace(/[\r\n]+/gm, "" );
-            a.setAttribute('href',encodeURI(`./src/instruccion.html?titulo=${fila}&paso=1`));
+            if(fila=="Collaborate"){
+                a.setAttribute('href',encodeURI(`./src/${fila}.html`));
+            } else {
+                a.setAttribute('href',encodeURI(`./src/instruccion.html?titulo=${fila}&paso=1`));
+            }
             a.setAttribute('class',"list-group-item list-group-item-action");
             a.textContent = fila
             lista.appendChild(a)
@@ -16,7 +20,7 @@ function cargar_apartados(ruta){
     })
     .catch(e => {
         console.error("Error "+e);
-        console.warn("Probando con ruta completa al archivo: https://j0nan.github.io/PAT/Practica3/res/contenido.txt");
+        console.warn("Probando con ruta completa al archivo de contenido: https://j0nan.github.io/PAT/Practica3/res/contenido.txt");
         cargar_apartados('https://j0nan.github.io/PAT/Practica3/res/contenido.txt')
     });
 }
