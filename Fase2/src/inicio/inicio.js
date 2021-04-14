f7caf2382be8e9fba7733ba4e05584cc0bc8d2be
J0nan <jagilcarrera@gmail.com>
2021-04-14 13:30:18 +0200

    Añadido opcion para el hostname

--- Fase2/src/inicio/inicio.js
+++ Fase2/src/inicio/inicio.js
@@ -34,8 +34,19 @@ function cargar_apartados(ruta, intentado=0){
     .catch(e => {
         console.error("Error "+e);
         if( intentado == 0){
-            console.warn("Probando con ruta completa al archivo de contenido: https://j0nan.github.io/PAT/Practica3/res/contenido.txt");
+            console.warn("Probando con ruta completa al archivo de contenido: https://j0nan.github.io/PAT/Fase2/res/contenido.txt");
             cargar_apartados('https://j0nan.github.io/PAT/Fase2/res/contenido.txt',1)
         }
     });
+}
+
+function GetURLParameters(sParam){
+    var sPageURL = decodeURIComponent(window.location.search.substring(1));
+    var sURLVariables = sPageURL.split('&');
+    for(var i = 0; i<sURLVariables.length; i++){
+        var sParameterName = sURLVariables[i].split('=');
+        if(sParameterName[0]==sParam){
+            return sParameterName[1];
+        }
+    }
 }
---
+++ Fase2/start-web.cmd
@@ -0,0 +1,4 @@
+:START
+start https://j0nan.github.io/PAT/Fase2?computername=%computername%
+pause
+goto START
