b600d8eb5419b2b495333b226243c9e1ab854997
J0nan <jagilcarrera@gmail.com>
2021-04-30 22:34:33 +0200

    Añadido cambio de contraseña en el front

--- Practica6/microservices/src/main/resources/static/js/change-password.js
+++ Practica6/microservices/src/main/resources/static/js/change-password.js
@@ -2,7 +2,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     tokenVerification();
 
-    loadingContents();
+    var form = document.getElementById("myform");
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        return sendForm();
+    });
 });
 
 function tokenVerification() {
@@ -15,6 +19,40 @@ function tokenVerification() {
     console.log("Cookie detected");
 }
 
-function loadingContents() {
-    console.log("No contents to load")
+function sendForm() {
+    try {
+        var inputValue1 = document.getElementById("current_password").value;
+        var inputValue2 = document.getElementById("new_password1").value;
+        var inputValue3 = document.getElementById("new_password2").value;
+        const data = { currentPassword: inputValue1, newPassword: inputValue2, newPassword2: inputValue3 };
+        const address = '/api/users/update-password';
+        fetch(address, {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer '+Cookies.get('token'),
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+            })
+            .then(response => {
+                if (response.ok) {
+                    alert("Contraseña cambiada")
+                    document.location.href="/home.html";
+                } else if(response.status==401){ //Unauthorized
+                    alert("Contraseña erronea");
+                    document.getElementById("current_password").style="border: 3px solid red"
+                    document.getElementById("new_password1").style="border: 1px solid black"
+                    document.getElementById("new_password2").style="border: 1px solid black"
+                } else if(response.status==400){ //BAD_REQUEST
+                    alert("Contraseñas no coinciden")
+                    document.getElementById("current_password").style="border: 1px solid black"
+                    document.getElementById("new_password1").style="border: 3px solid red"
+                    document.getElementById("new_password2").style="border: 3px solid red"
+                }
+            });
+
+    } catch (err) {
+        console.error(err.message);
+    }
+    return false;
 }
